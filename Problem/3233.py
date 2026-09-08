6ab57a1e57687e38da355de90c787c255a9d269f
LeerMaiDai <69070322@kmitl.ac.th>
2026-09-08 23:12:44 +0700

    Hellnah bro

--- Problem/3233.py
+++ Problem/3233.py
@@ -1,20 +1,20 @@
 """luck"""
-t, t1 = map(str, input().split())
-f, f1 = map(str, input().split())
+t, t1 = input().split()
+f, f1 = input().split()
 
 if t == f and t1 == f1:
     print(1000000)
-elif t == f and t1 != f1:
-    print(0)
 elif t != f and t1 == f1:
     print(100000)
-elif t == f and t1[-3:] == f1[-3:]:
+elif t == f and t1[2:5] == f1[2:5]:
     print(2000)
-elif t == f and t1[-2:] == f1[-2:]:
+elif t == f and t1[3:5] == f1[3:5]:
     print(1000)
-elif t != f and t1[-3:] == f1[-3:]:
+elif t != f and t1[2:5] == f1[2:5]:
     print(200)
-elif t != f and t1[-2:] == f1[-2:]:
+elif t != f and t1[3:5] == f1[3:5]:
     print(100)
+elif t == f and t1 != f1:
+    print(20)
 elif t != f and t1 != f1:
     print(0)
